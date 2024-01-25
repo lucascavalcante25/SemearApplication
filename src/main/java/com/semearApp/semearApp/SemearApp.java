e0a61dcc81cff6f5145fb66d4c1387f9a1be7c97
Lucas Cavalcante <52172991+lucascavalcante25@users.noreply.github.com>
2024-01-25 00:53:18 -0300

    configuracao de variaveis de ambiente

--- src/main/java/com/semearApp/semearApp/SemearApp.java
+++ src/main/java/com/semearApp/semearApp/SemearApp.java
@@ -7,7 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SemearApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SemearApp.class, args);
-	}
+        // Log das variáveis de ambiente
+        System.out.println("DATABASE_URL: " + System.getenv("DATABASE_URL"));
+        System.out.println("DATABASE_USERNAME: " + System.getenv("DATABASE_USERNAME"));
+        System.out.println("DATABASE_PASSWORD: " + System.getenv("DATABASE_PASSWORD"));
+
+        SpringApplication.run(SemearApp.class, args);
+    }
 }
 
